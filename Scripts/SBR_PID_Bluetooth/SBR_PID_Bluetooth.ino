e32638775534dc06c55b4f4972751a28fbe2299c
ImRayx <kevinhartmann2904@web.de>
2026-06-08 21:09:19 +0200

    Major changes in pid controller and bluetooth connection for ps4 controller

--- Scripts/SBR_PID_Bluetooth/SBR_PID_Bluetooth.ino
+++ Scripts/SBR_PID_Bluetooth/SBR_PID_Bluetooth.ino
@@ -2,9 +2,10 @@
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 #include <Bluepad32.h>
+//For my esp32 select DOIT ESP32 DEVKIT V1 board
 
-ControllerPtr myControllers[BP32_MAX_GAMEPADS]; //for the ps4 controller
-// Pin Definitions (Matches your KiCad Schematic)
+ControllerPtr myControllers[BP32_MAX_GAMEPADS]; //PS4-Controller object
+// Pin Definitions
 #define LEFT_STEP_PIN  16
 #define LEFT_DIR_PIN   17
 #define LEFT_EN_PIN 25
@@ -15,11 +16,12 @@ ControllerPtr myControllers[BP32_MAX_GAMEPADS]; //for the ps4 controller
 #define RIGHT_EN_PIN 2
 #define RIGHT_MS1_PIN 12
 #define RIGHT_MS2_PIN 14
-// Define the stepper interface type (1 = Driver with Step/Dir pins)
+//Stepper interface type (1 = Driver with Step/Dir pins)
 #define MOTOR_INTERFACE_TYPE 1
 #define BATTERY_VOLTAGE_PIN 15  //reads battery voltage in order to prevent undercharging
 
-const float R1 = 32000.0;
+//Constants for battery voltage calculation
+const float R1 = 32000.0; 
 const float R2 = 10000.0;
 const float RATIO = (R1 + R2) / R2;  // = 4.3
 const float ADC_REF = 3.3;
@@ -30,18 +32,18 @@ AccelStepper leftMotor(MOTOR_INTERFACE_TYPE, LEFT_STEP_PIN, LEFT_DIR_PIN);
 AccelStepper rightMotor(MOTOR_INTERFACE_TYPE, RIGHT_STEP_PIN, RIGHT_DIR_PIN);
 MPU6050 mpu6050(Wire);
 
-//PID Constants
-float kp = 30.0;   // Start small (try 10-50)
-float ki = 0.1;    // Start very small (try 0.1-1.0)
-float kd = 2.5;    // Start small (try 1.0-5.0)
-
-float targetAngle = 0.0; // The "Perfectly Level" goal
-float error, lastError, integratedError, derivative;
+//PID Constants (400.0, 1.0, 180.0 works best so far)
+float kp = 400.0;
+float ki = 0.0;   
+float kd = 180.0;
+//Variables for error calculations
+float targetAngle = 0.0; //PID controller tries to control the robot to stay at 0 degrees -> upright position
+float error, lastError, integratedError, derivative;  
 float motorSpeed;
 
 unsigned long lastTime;
 
-//##########BatteryVoltage################
+//Battery reading funtion
 float readBatteryVoltage() {
     long sum = 0;
     for (int i = 0; i < 64; i++) {
@@ -52,7 +54,8 @@ float readBatteryVoltage() {
     float voltage = vout * RATIO;
     return voltage * (10.5 / 12.8); // correction factor = 0.777
 }
-//#############PS4_CONTROLLER######################
+
+//PS4-Controller functions
 void onConnectedController(ControllerPtr ctl) {
     if (myControllers[0] == nullptr) {
         Serial.println("Controller connected!");
@@ -75,155 +78,130 @@ float processGamepad(ControllerPtr ctl) {
     int r2 = ctl->throttle();   // Left stick Y-axis (forward/backward)
     int l2 = ctl->brake();   // Left stick X-axis (left/right)
     int angleCorrection = 0;
-    // Simple tank drive control
+    //Driving Controls
     if (r2 > 0) {
         // Forward
-        int angleCorrection = map(abs(r2), 0, 1023, 0, 5);  //this translates the joystick to a 5 degree angle
-        Serial.println(angleCorrection);
+        int angleCorrection = map(abs(r2), 0, 1023, 0, 5);  // Translates the joystick input range to a 5 degree angle range
+        //Serial.println(angleCorrection);
         return angleCorrection;
     } 
     else if (l2 > 0) {
         // Reverse
-        int angleCorrection = map(abs(l2), 0, 1023, 0, -5);  //this translates the joystick to a 5 degree angle
-        Serial.println(angleCorrection);
+        int angleCorrection = map(abs(l2), 0, 1023, 0, -5);
+        //Serial.println(angleCorrection);
         return angleCorrection;
     }
     else{
       targetAngle= 0.0;
       angleCorrection = 0; 
     }
-    // Button example - emergency stop
+    // Emergency Button
     if (ctl->a()) { 
-        // Stop all motors, if x button on ps4-controller gets pressed
+        // Kills all motors, if x button on ps4-controller gets pressed
         digitalWrite(LEFT_EN_PIN, HIGH);  //EN_PINS get activated to shut off motors
         digitalWrite(RIGHT_EN_PIN, HIGH);
     }
     return angleCorrection;
 }
 
-
-
 void setup() {
- // Set a constant speed (Steps per second)
-  // 800 steps/sec = 1/4 turn per second at 1/16 microstepping
   Serial.begin(115200);
   //PS4_CONTROLLER SETUP
   BP32.setup(&onConnectedController, &onDisconnectedController);
-  BP32.forgetBluetoothKeys();  // Optional: clear previous pairings
-
-  Wire.begin(); // Starts I2C communication
-  
+  BP32.forgetBluetoothKeys();  // Clears previous bluetooth pairings
+  Wire.begin();                // Starts I2C communication for Gy-86
   mpu6050.begin();
-  // This calculates the 'offset' so your robot knows what 'zero' is.
-  // Make sure the GY-87 is perfectly level during this!
-  mpu6050.calcGyroOffsets(true); 
-  
+  mpu6050.calcGyroOffsets(true); // Calculates offset in order to know what real 'zero' is; Robot should be upright for this adjustment
   Serial.println("Done!");
 
-
-  leftMotor.setMaxSpeed(10000);  // Higher ceiling
-
+  //Max speed for the stepper motors
+  leftMotor.setMaxSpeed(10000);  
   rightMotor.setMaxSpeed(10000);
 
+  //PINMODES
   pinMode(LEFT_EN_PIN, OUTPUT); 
   pinMode(RIGHT_EN_PIN, OUTPUT);
-  pinMode(LEFT_MS1_PIN, OUTPUT);  //The combination of HIGH and LOW MS_PINS decides on the step size of the motors
-  pinMode(RIGHT_MS1_PIN, OUTPUT);
+  pinMode(LEFT_MS1_PIN, OUTPUT);  
+  pinMode(RIGHT_MS1_PIN, OUTPUT); 
   pinMode(LEFT_MS2_PIN, OUTPUT);
   pinMode(RIGHT_MS2_PIN, OUTPUT);
   pinMode(BATTERY_VOLTAGE_PIN, INPUT);
-
-  digitalWrite(LEFT_EN_PIN, LOW); //Turns on the motors
+  //EN PINS
+  digitalWrite(LEFT_EN_PIN, LOW); // Turns on the motors if set on LOW
   digitalWrite(RIGHT_EN_PIN, LOW);
-
-  digitalWrite(LEFT_MS1_PIN, LOW);  //The combination of HIGH and LOW MS_PINS decides on the step size of the motors
+  //MS PINS
+  digitalWrite(LEFT_MS1_PIN, LOW);  // The combination of HIGH and LOW MS_PINS decides on the step size of the motors
   digitalWrite(RIGHT_MS1_PIN, LOW);
   digitalWrite(LEFT_MS2_PIN, LOW);
   digitalWrite(RIGHT_MS2_PIN, LOW);
+  //The combination of HIGH and LOW MS_PINS decides on the step size of the motors -> TMC2209-Datasheet
 
 }
 void loop() {
-  mpu6050.update(); //updates gyroscope
-  unsigned long currentTime = millis(); //tracks time
-
-  BP32.update();  //updates ps4-controller
+  mpu6050.update(); //Updates gyroscope
+  unsigned long currentTime = millis(); //Tracks time in ms
+  BP32.update();  //Updates PS4-controller inputs
   for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
       ControllerPtr ctl = myControllers[i];
       if (ctl && ctl->isConnected() && ctl->hasData()) {
           targetAngle = processGamepad(ctl);
       }
   }
-  
-  // PID runs every 5ms
-  if (currentTime - lastTime >= 5) {
-    float currentAngle = mpu6050.getAngleX();
-    
-    // 1. Calculate Error
+  //PID calculation at 500Hz
+  if (currentTime - lastTime >= 2) {
+    float currentAngle = mpu6050.getAngleX(); // Gets X-angle from the gy-86
+    Serial.println(currentAngle); 
+    // Error calculation
     error = currentAngle - targetAngle;
-    
-    // 2. Proportional Term
+    // Partial term
     float P = kp * error;
-    
-    // 3. Integral Term
+    // Integral term
     integratedError += error;
     integratedError = constraint(integratedError, -500, 500); 
     float I = ki * integratedError;
-    
-    // 4. Derivative Term
+    // Derivative term
     derivative = error - lastError;
     float D = kd * derivative;
-    
-    // 5. Total Output
+    // PID-Output
     motorSpeed = P + I + D;
     
-    // Safety: Kill motors if tipped too far
+    // Kill motors if tipping angle goes over 40 degrees
     if (abs(currentAngle) > 40) {
-      digitalWrite(LEFT_EN_PIN, HIGH);
+      digitalWrite(LEFT_EN_PIN, HIGH);  // Kills motors by setting the enable pins on the motor drivers to HIGH to turn them off
       digitalWrite(RIGHT_EN_PIN, HIGH);
-      motorSpeed = 0;
+      motorSpeed = 0;                   // In case that turn off doesnt work, the motorspeed is set to 0
     } else {
       digitalWrite(LEFT_EN_PIN, LOW);
       digitalWrite(RIGHT_EN_PIN, LOW);
     }
 
-    // Apply to motors
+    //Apply speed to motors
     leftMotor.setSpeed(-motorSpeed);
     rightMotor.setSpeed(motorSpeed);
-    
     lastError = error;
     lastTime = currentTime;
-    
-    // Debug output (optional - can slow things down)
-    //Serial.print("Angle: "); Serial.print(currentAngle);
-    //Serial.print(" | Speed: "); Serial.println(motorSpeed);
   }
-
-  // CRITICAL: These must run every loop iteration
-  leftMotor.runSpeed();
+  leftMotor.runSpeed(); //Starts to move the motors with setSpeed
   rightMotor.runSpeed();
   
-  // Battery check - only once per second, NON-BLOCKING
+  // Battery check every second
   static unsigned long lastBatteryCheck = 0;
   if (currentTime - lastBatteryCheck >= 1000) {
     static int sampleCount = 0;
     static long voltageSum = 0;
-    
-    // Take one sample per loop, accumulate over time
+    //One sample per loop -> tracks the voltage over time
     if (sampleCount < 64) {
       voltageSum += analogRead(BATTERY_VOLTAGE_PIN);
-      sampleCount++;
+      sampleCount++;  //increase sample count
     } else {
       // Calculate voltage from accumulated samples
-      float vout = ((voltageSum / 64.0) / ADC_RES) * ADC_REF;
+      float vout = ((voltageSum / 64.0) / ADC_RES) * ADC_REF; //gets the average battery voltage over 64 samples 
       float voltage = vout * RATIO * (10.5 / 12.8);
-      
       Serial.print("Battery: "); Serial.println(voltage);
-      
-      if(voltage <= 9.0) {
+      if(voltage <= 9.0) {    //turns off the motors if battery voltage is under 9V
         digitalWrite(LEFT_EN_PIN, HIGH);
         digitalWrite(RIGHT_EN_PIN, HIGH);
       }
-      
       // Reset for next reading
       sampleCount = 0;
       voltageSum = 0;
@@ -231,7 +209,6 @@ void loop() {
     }
   }
 }
-
 
 // Simple helper to keep numbers in range
 float constraint(float val, float minVal, float maxVal) {
